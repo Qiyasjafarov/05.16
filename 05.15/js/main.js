24706f4184c59117d11cac09a777fdb59d0cac84
Qiyasjafarov <100667434+Qiyasjafarov@users.noreply.github.com>
2022-05-15 21:53:29 +0400

    Add files via upload

---
+++ 05.15/js/main.js
@@ -0,0 +1,29 @@
+"use strict"
+
+
+// const arr = [45, 4, 9, 16, 25];
+// arr.forEach(x=>{console.log(x);})
+
+
+
+// const numbers1 = [45, 4, 9, 16, 25];
+// const numbers2 = numbers1.map(x=>{console.log(x*2);});
+
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// const over18 = numbers.filter(x=>{console.log(x>5);});
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// let sum = numbers.reduceRight(x=>{return(x+1);});
+// console.log(sum);
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// let allOver18 = numbers.every(x=>{console.log(x>25);});
+
+// const numbers = [45, 4, 9, 16, 25];
+// let someOver18 = numbers.some(x=>{console.log(x>25);});
+
+
